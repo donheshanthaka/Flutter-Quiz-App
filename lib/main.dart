aaaff23cc3eba03be318f06f16c67b548edaf039
Don Heshanthaka <heshanthaka007@gmail.com>
2022-06-21 09:23:09 +0530

    Implementation of conditional statement to check the number of questions

--- lib/main.dart
+++ lib/main.dart
@@ -16,36 +16,64 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
+  final questions = const [
+      {
+        'questionText': 'What\'s your favorite color?',
+        'answers': ['Black', 'Red', 'Green', 'White'],
+      },
+      {
+        'questionText': 'What\'s your favorite animal?',
+        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+      },
+      {
+        'questionText': 'Who\'s your favorite instructor?',
+        'answers': ['Max', 'Jane', 'Done', 'Mike'],
+      },
+    ];
+
   var _questionIndex = 0;
 
   void _answerQuestion() {
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
+
+    if (_questionIndex < questions.length){
+      print('we have more questions');
+    } else {
+      print('Out of questions');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s your favorite color',
-      'What\'s your favorite animal',
-    ];
+    
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
+        body: _questionIndex < questions.length ? Column(
           children: [
             Question(
-              questions[_questionIndex],
+              questions[_questionIndex]['questionText'],
             ),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
+            // We are transforming every answer into an Answer widget, convert them into a list and then add them to the children list.
+            // By using the ... we are expanding the list below, such that the elements inside the list(Answer widgets) will be added to the children list,
+            // instead of adding a list inside the list
+            // it will be like...
+            // Answer(_answerQuestion, answer),
+            // Answer(_answerQuestion, answer),
+            // Answer(_answerQuestion, answer),
+            // ...
+            ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList(),
           ],
-        ),
+        ) : Center(child: Text('You did it!'),),
       ),
     );
   }
